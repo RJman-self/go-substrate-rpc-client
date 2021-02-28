97f62a6fef09d7068e9b769ef9529f95a19422ac
rjman-self <306714577@qq.com>
2021-02-28 14:19:24 +0800

    update Multi

--- types/extrinsic.go
+++ types/extrinsic.go
@@ -119,58 +119,6 @@ func (e Extrinsic) Type() uint8 {
 	return e.Version & ExtrinsicUnmaskVersion
 }
 
-//
-//// Sign adds a signature to the extrinsic
-//func (e *Extrinsic) Sign(signer signature.KeyringPair, o SignatureOptions) error {
-//	if e.Type() != ExtrinsicVersion4 {
-//		return fmt.Errorf("unsupported extrinsic version: %v (isSigned: %v, type: %v)", e.Version, e.IsSigned(), e.Type())
-//	}
-//
-//	mb, err := EncodeToBytes(e.Method)
-//	if err != nil {
-//		return err
-//	}
-//
-//	era := o.Era
-//	if !o.Era.IsMortalEra {
-//		era = ExtrinsicEra{IsImmortalEra: true}
-//	}
-//
-//	payload := ExtrinsicPayloadV4{
-//		ExtrinsicPayloadV3: ExtrinsicPayloadV3{
-//			Method:      mb,
-//			Era:         era,
-//			Nonce:       o.Nonce,
-//			Tip:         o.Tip,
-//			SpecVersion: o.SpecVersion,
-//			GenesisHash: o.GenesisHash,
-//			BlockHash:   o.BlockHash,
-//		},
-//		TransactionVersion: o.TransactionVersion,
-//	}
-//
-//	signerPubKey := NewAddressFromAccountID(signer.PublicKey)
-//
-//	sig, err := payload.Sign(signer)
-//	if err != nil {
-//		return err
-//	}
-//
-//	extSig := ExtrinsicSignatureV4{
-//		Signer:    signerPubKey,
-//		Signature: MultiSignature{IsSr25519: true, AsSr25519: sig},
-//		Era:       era,
-//		Nonce:     o.Nonce,
-//		Tip:       o.Tip,
-//	}
-//
-//	e.Signature = extSig
-//
-//	// mark the extrinsic as signed
-//	e.Version |= ExtrinsicBitSigned
-//
-//	return nil
-//}
 func (e *Extrinsic) MultiSign(signer signature.KeyringPair, o SignatureOptions) error {
 	if e.Type() != ExtrinsicVersion4 {
 		return fmt.Errorf("unsupported extrinsic version: %v (isSigned: %v, type: %v)", e.Version, e.IsSigned(), e.Type())
